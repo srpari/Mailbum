1097fc7a260e4c611c3d35029f3f3c78cd26cc00
Parimala <pari.ramamoorthy@gmail.com>
2020-06-25 20:17:51 -0400

    Implementing Javascript for Toggle Checkbox Function

---
+++ resourses/js/script.js
@@ -0,0 +1,26 @@
+function showHtmlScript() {
+		
+		if(document.getElementById("showhtml").checked){		
+		   document.getElementById("htmlscript").style.display="block";}
+		else {		
+		  document.getElementById("htmlscript").style.display="none";}	
+	}
+		
+function showSocialIcons() {
+	   if(document.getElementById("socialIcon").checked){		
+		 document.getElementById("socialIconGroup").style.display="block";}
+		else {		
+		  document.getElementById("socialIconGroup").style.display="none";}
+	
+	}
+
+
+/*
+function showHtmlScript() {
+		var x = document.getElementById("htmlscript");
+		 if (x.style.display === "none") {
+			 x.style.display = "block";
+		  } else {
+			x.style.display = "none";
+		  }
+	}  */
