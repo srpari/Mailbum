19b451b25204ee5510c4a904188e45a43da31937
Parimala <pari.ramamoorthy@gmail.com>
2020-07-07 22:40:59 -0400

    Logo & SocialIcons Disable-JS

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -60,49 +60,79 @@ function printWebsite(){
 	document.getElementById("printwebsite").innerHTML=x;	
 }
 
-function printLogourl(){	
-	document.getElementById("logoimg").style.display="block";
-	document.getElementById('logoimg').src =document.getElementById("logourl").value;	
+function printLogourl(){
+	var len=document.getElementById("logourl").value.length;
+	if (len==0){
+    	document.getElementById("logoimg").style.display="none";
+	}else {
+		document.getElementById("logoimg").style.display="block";
+		document.getElementById('logoimg').src =document.getElementById("logourl").value;
+	}	
 }
 
+
 function printFacebook() {
-	document.getElementById("fbimg").style.display="inline-block";
-	var x=document.getElementById("facebook").value;	
-	document.getElementById("printfacebook").href=x; 	
-	var slidervalue=document.getElementById("sliderrange").value;	
-	document.getElementById("fbimg").width=slidervalue;
+	var x=document.getElementById("facebook").value;
+	var len=document.getElementById("facebook").value.length;
+	if (len==0){
+    	document.getElementById("fbimg").style.display="none";
+	} else {			
+		document.getElementById("fbimg").style.display="inline-block";
+		document.getElementById("printfacebook").href=x; 	
+		var slidervalue=document.getElementById("sliderrange").value;	
+		document.getElementById("fbimg").width=slidervalue;
+	}
 }
 
 function printTwitter() {
-	document.getElementById("twimg").style.display="inline-block";
-	var x=document.getElementById("twitter").value;	
-	document.getElementById("printtwitter").href=x; 	
-	var slidervalue=document.getElementById("sliderrange").value;	
-	document.getElementById("twimg").width=slidervalue;
+	var x=document.getElementById("twitter").value;	var len=document.getElementById("twitter").value.length;
+	if (len==0){
+    	document.getElementById("twimg").style.display="none";
+	} else {		
+		document.getElementById("twimg").style.display="inline-block";
+		document.getElementById("printtwitter").href=x; 	
+		var slidervalue=document.getElementById("sliderrange").value;	
+		document.getElementById("twimg").width=slidervalue;
+	}
 }
 
 function printInstagram() {
+	var x=document.getElementById("instagram").value;
+	var len=document.getElementById("instagram").value.length;
+	if (len==0){
+    	document.getElementById("instaimg").style.display="none";
+	} else {	
 	document.getElementById("instaimg").style.display="inline-block";
-	var x=document.getElementById("instagram").value;	
 	document.getElementById("printinstagram").href=x; 	
 	var slidervalue=document.getElementById("sliderrange").value;	
 	document.getElementById("instaimg").width=slidervalue;
+	}
 }
 
 function printYoutube() {
-	document.getElementById("youtubeimg").style.display="inline-block";
 	var x=document.getElementById("youtube").value;	
-	document.getElementById("printyoutube").href=x; 	
-	var slidervalue=document.getElementById("sliderrange").value;	
-	document.getElementById("youtubeimg").width=slidervalue;
+	var len=document.getElementById("youtube").value.length;
+	if (len==0){
+    	document.getElementById("youtubeimg").style.display="none";
+	} else {
+		document.getElementById("youtubeimg").style.display="inline-block";
+		document.getElementById("printyoutube").href=x; 	
+		var slidervalue=document.getElementById("sliderrange").value;	
+		document.getElementById("youtubeimg").width=slidervalue;
+	}
 }
 
 function printLinkedin() {
-	document.getElementById("linkedinimg").style.display="inline-block";
 	var x=document.getElementById("linkedin").value;	
-	document.getElementById("printlinkedin").href=x; 	
-	var slidervalue=document.getElementById("sliderrange").value;	
-	document.getElementById("linkedinimg").width=slidervalue;
+	var len=document.getElementById("linkedin").value.length;
+	if (len==0){
+    	document.getElementById("linkedinimg").style.display="none";
+	} else {
+		document.getElementById("linkedinimg").style.display="inline-block";
+		document.getElementById("printlinkedin").href=x; 	
+		var slidervalue=document.getElementById("sliderrange").value;	
+		document.getElementById("linkedinimg").width=slidervalue;
+	}
 }
 
 
